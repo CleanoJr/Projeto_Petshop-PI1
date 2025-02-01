92bc9854e0c36113e7ceee12b4e4bdd42d9d0e58
Uriel Nacimento <102154454+urielnasciment0@users.noreply.github.com>
2025-02-01 12:14:16 -0300

    Implementação do cadastro de usuário
    Criação da conexão com o banco, usuario _model e usuario _controller.

--- controllers/usuario_controller.py
+++ controllers/usuario_controller.py
@@ -1,7 +1,39 @@
 from main import app
-from flask import render_template
+from flask import request, render_template, redirect, url_for
+from models.usuario_model import *
+# Criando a sessão para interagir com o banco de dados
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Rotas
-@app.route('/')
-def index():
-    return render_template('login.html')
+
+# rotas
+@app.route("/usuario/inserir")
+def inserir():
+    return render_template("usuario/create.html")
+
+
+@app.route("/usuario/inserir", methods=['POST'])
+def create():   
+    if request.method == 'POST':      
+        # Captura os dados enviados pelo formulário        
+        nome = request.form['nome']      
+        login = request.form['login']
+        senha = request.form['senha']      
+        email = request.form['email']
+        telefone = request.form['telefone']
+
+        # Cria um novo usuário      
+        new_user = Usuario(nome=nome, login=login, senha=senha, email=email, telefone=telefone)      
+        
+        # Cria a sessão
+        db = SessionLocal()
+        
+        # Adiciona o novo usuário ao banco de dados
+        db.add(new_user)
+        db.commit()
+        
+        # Retorna para página de login
+        return redirect(url_for('login'))
+
+@app.route("/")
+def login():
+    return render_template("login.html")
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask
 # Criação de uma instância do Flask
 app = Flask(__name__)
 
-from usuario_controller import *
+from controllers.usuario_controller import *
 
 #Inicia o servidor de desenvolvimento.
 if __name__ == '__main__':
